0855a00a805902103d2663804d711237c840ba7c
Ilya-bunat <ilya.binat15@gmail.com>
2025-06-28 16:07:53 +0300

    models create

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render # идет по умолчанию
 
+
+def home(request):
+    return render(request, 'home.html') #  Функция открытия нашей стратовой транице
 # Create your views here.
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # include - соединяет два роутера (соединяет app c нашим проектом)
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("app", include(("app.urls", "app"), namespace="app")),
+    path("", include(("app.urls", "app"), namespace="app")),
 ]
+urlpatterns += staticfiles_urlpatterns() # Встроенная в  django функция которая найдет в settings.py адресс папки со статичными файлами 
+#и настроит открытие этих файлов через браузер. 
