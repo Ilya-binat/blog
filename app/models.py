f55feb66a2f5b609a204a3dc9bfa552866ac02f4
Ilya-bunat <ilya.binat15@gmail.com>
2025-06-29 14:37:39 +0300

    post commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
@@ -8,3 +10,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title # возвращает заголовки постов, если поставить self .body вернется тело поста
+    
+    def snippet(self):
+        return self.body[:30] + '...'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render # идет по умолчанию
 
+from.models import Post
+
 
 def home(request):
-    return render(request, 'home.html') #  Функция открытия нашей стратовой транице
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts':posts}) #  Функция открытия нашей стратовой транице
+
+
 # Create your views here.
+
